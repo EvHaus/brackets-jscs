3d44672f5d83dddf4d58b6b450ba2cad5ad55e64
globexdesigns <evgueni@globexdesigns.com>
2013-12-02 20:43:34 -0800

    0.0.1 - Initial release

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 brackets-jscs
-=============
+=================
 
-Brackets extension for JSCS
+A Brackets extension to enable JSCS support.
+
+JSHint can be configured configured by .jscsrc.json file located the project root.
+
+Issues/Updates
+=====
+
+**[2013-02-12]** Initial release. Currently doesn't work while JSHint is enabled.
+
+Credit
+=====
+
+Based heavily on [brackets-jshint](https://github.com/cfjedimaster/brackets-jshint/).
---
+++ main.js
@@ -0,0 +1,149 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets */
+
+define(function (require, exports, module) {
+	"use strict";
+
+	var AppInit = brackets.getModule("utils/AppInit"),
+		CodeInspection = brackets.getModule("language/CodeInspection"),
+		FileSystem = brackets.getModule("filesystem/FileSystem"),
+		ProjectManager = brackets.getModule("project/ProjectManager"),
+		DocumentManager = brackets.getModule("document/DocumentManager"),
+		
+		JscsStringChecker = require("jscs/jscs-browser"),
+		
+		_configFileName = ".jscs.json",
+		
+		config = {
+			disallowMultipleVarDecl: true
+		};
+
+	
+	// JSCSParser()
+	// Main function for the Brackets Linting API
+	//
+	// @param	text		string		The string of code to validate
+	// @param	fullpath	string		File path to the file
+	//
+	function JSCSParser(text, fullPath) {
+		
+		// Initialize JSCS
+		try {
+			var checker = new JscsStringChecker();
+			checker.registerDefaultRules();
+			checker.configure(config);
+		} catch (e) {
+			console.error("JSCS Failed to initialize: " + e);
+			return;	
+		}
+		
+		// Run JSCS
+		var errors = checker.checkString(text),
+			errList = errors.getErrorList();
+		
+		var result = {
+			errors: []
+		};
+		
+		// Get errors
+		if (errList.length) {
+			errList.forEach(function(error) {
+				result.errors.push({
+					pos: {
+						line: error.line,
+						ch: error.column
+					},
+					message: error.message,
+					type: CodeInspection.Type.WARNING
+				});
+			});
+			return result;
+		} else {
+			
+			return null;	
+		}
+	}
+	
+
+	// _loadProjectConfig()
+	// Loads project-wide JSCS configuration.
+	//
+	// JSCS project file should be located at <Project Root>/.jscs.json. It
+	// is loaded each time project is changed or the configuration file is
+	// modified.
+	//
+	// @return Promise to return JSCS configuration object.
+	//
+	function _loadProjectConfig() {
+
+		var projectRootEntry = ProjectManager.getProjectRoot(),
+			result = new $.Deferred(),
+			file,
+			config;
+		
+		file = FileSystem.getFileForPath(projectRootEntry.fullPath + _configFileName);
+		file.read(function (err, content) {
+			if (!err) {
+				var cfg = {};
+				
+				try {
+					config = JSON.parse(content);
+				} catch (e) {
+					console.error("JSCS: Error parsing " + file.fullPath + ". Details: " + e);
+					result.reject(e);
+					return;
+				}
+				
+				result.resolve(config);
+			} else {
+				result.reject(err);
+			}
+		});
+		return result.promise();
+	}
+	
+
+	// tryLoadConfig()
+	// Attempts to load project configuration file.
+	//
+	function tryLoadConfig() {
+		function _refreshCodeInspection() {
+			CodeInspection.toggleEnabled();
+			CodeInspection.toggleEnabled();
+		}
+		
+		_loadProjectConfig()
+			.done(function (newConfig) {
+				config = newConfig;
+			})
+			.fail(function () {
+				config = defaultConfig;
+			})
+			.always(function () {
+				_refreshCodeInspection();
+			});
+	}
+
+	AppInit.appReady(function () {
+		
+		CodeInspection.register("javascript", {
+			name: "JSCS",
+			scanFile: JSCSParser
+		});
+		
+		$(DocumentManager)
+            .on("documentSaved.jscs documentRefreshed.jscs", function (e, document) {
+                if (document.file.fullPath === ProjectManager.getProjectRoot().fullPath + _configFileName) {
+                    tryLoadConfig();
+                }
+            });
+        
+        $(ProjectManager)
+            .on("projectOpen.jscs", function () {
+                tryLoadConfig();
+            });
+        
+        tryLoadConfig();
+	});
+
+});
